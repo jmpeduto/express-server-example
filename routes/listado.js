7ab67c238825997ad21d8dbb55918c149a235886
Juan Peduto <jmpeduto@gmail.com>
2021-12-20 15:58:04 -0300

    cambios llamada got

--- routes/listado.js
+++ routes/listado.js
@@ -1,7 +1,9 @@
 var express = require('express');
-const axios = require('axios');
+// const axios = require('axios');
+// const superagent = require('superagent');
 // import got from {got};
 var http = require('http');
+const got = require('got');
 var router = express.Router();
 // Parse JSON bodies for this app. Make sure you put
 // `app.use(express.json())` **before** your route handlers!
@@ -9,31 +11,37 @@ var router = express.Router();
 // Demo showing the server in action
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, resp, next) {
 	console.log('entra a server');
-	// app.get("/getListado", (req, resp) => {
-	// 1. Crea un nuevo objeto XMLHttpRequest
-	// req.body; // JavaScript object containing the parse JSON
-	var respuesta = {'vacio': ''};
-	const respaxios = axios.post('http://c1300044.ferozo.com/getListado.php', {
-	}).then( resp => {
-		// console.log(resp);
-		respuesta = resp;
-	});
-    // // res.json(req.body);
-	// return respaxios.data;
-	//     http.post('http://c1300044.ferozo.com/getListado.php', { retry: { limit: 3, methods: ["GET", "POST"] }, json: true }).then(response_ => {
-	//       console.log(response_.body.url);
-	//    //    console.log(response.body_.explanation);
-	//       response = response_;
-	//     }).catch(error => {
-	//       console.log(error);
-	//     });
+	var respuesta = { 'vacio': '' };
+	// const respaxios = axios.post('http://c1300044.ferozo.com/getListado.php', {
+	// }).then( resp => {
+	// 	// console.log(resp);
+	// 	respuesta = resp;
+	// });
+	// //
 
-	// return resp.json();
-	//    });
+	// superagent
+	// 	.post('http://c1300044.ferozo.com/getListado.php')
+	// 	.query({ })
+	// 	.end((err, res) => {
+	// 		if (err) {
+	// 			return console.log(err);
+	// 		}
+	// 		respuesta = res;
+	// 		// console.log(res.body.url);
+	// 		// console.log(res.body.explanation);
+	// 	});
+
+	got('http://clarin.com', { json: true })
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 	console.log(respuesta);
-	res.send({'respuesta': 'respuestita'});
+	resp.send({ 'respuesta': 'respuestita' });
 });
 
 module.exports = router;
